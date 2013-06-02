02a4d2ed3766e6448e234f2f4e3e7769463ba08e
Łukasz Dubiel <bambucha14@gmail.com>
2013-06-02 06:53:14 +0200

    Name refactor. Extract protocol details to other function

--- main.py
+++ main.py
@@ -30,8 +30,11 @@ def print_joystic_axis_status(joystick):
     for i in range(joystick.get_numaxes()):
         print ("Format axis {} = {}".format(i,joystick.get_axis(i)))
 
-def prepar_message(left_engine,right_engine):
+def prepar_engine_speed(left_engine,right_engine):
     return b"{}\r{}\r".format(left_engine,right_engine)
+
+def change_engene_speed_message(left_engine,right_engine):
+    return b"v" + prepar_engine_speed(left_engine,right_engine)
 
 clock = pygame.time.Clock()
 serial = serial.Serial('/dev/rfcomm1',9600,timeout=1)
@@ -48,26 +51,25 @@ try:
 
         if (Xaxis != lastXaxis) or (Yaxis != lastYaxis):
             print ("X = {}, Y = {}".format(Xaxis,Yaxis))
-            serial.write(b'v')
             
-            to_send = prepar_message(0,0)
+            to_send = change_engine_speed_message(0,0)
             
             if(Xaxis == -1 and Yaxis == 0):
-                to_send = prepar_message(-255,-255)
+                to_send = change_engine_speed_message(-255,-255)
             elif(Xaxis == -1 and Yaxis == 1):
-                to_send = prepar_message(-255,0)
+                to_send = change_engine_speed_message(-255,0)
             elif(Xaxis == 0 and Yaxis == 1):
-               to_send =  prepar_message(-255,255)
+               to_send =  change_engine_speed_message(-255,255)
             elif(Xaxis == 1 and Yaxis == 1):
-                to_send = prepar_message(255,0)
+                to_send = change_engine_speed_message(255,0)
             elif(Xaxis == 1 and Yaxis == 0):
-                to_send = prepar_message(255,255)
+                to_send = change_engine_speed_message(255,255)
             elif(Xaxis == 1 and Yaxis == -1):
-                to_send = prepar_message(0,255)
+                to_send = change_engine_speed_message(0,255)
             elif(Xaxis == 0 and Yaxis == -1):
-                to_send =  prepar_message(255,-255)
+                to_send =  change_engine_speed_message(255,-255)
             elif(Xaxis == -1 and Yaxis == -1):
-                to_send = prepar_message(0,-255)
+                to_send = change_engine_speed_message(0,-255)
                 
             serial.write(to_send)
             print("wysyłane {}".format(to_send))
